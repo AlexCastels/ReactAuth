334718081b97e72a504123ba49388a4f3a1fe3f0
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-10-02 16:57:44 +0200

    Aggiunta condizione btn logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,28 @@
 import { Link, useNavigate } from 'react-router-dom'
 import styles from './navbar.module.css'
+import { useSelector } from 'react-redux'
 
 export function Navbar(){
 
     const navigate = useNavigate()
-
     const handleLogin = () => navigate('/login')
     const handleSignin = () => navigate('/signin')
+    const userState = useSelector(state => state.userState)
 
     return (
         <nav className={styles.container}>
             <div className={styles.navbar}>
                 <Link className={styles.titleLink} to={'/'}>ReactAuth Demo</Link>
                 <ul className={styles.list}>
-                    <li><button className={styles.listEl} onClick={handleLogin}>Log In</button></li>
-                    <li><button className={styles.listEl} onClick={handleSignin}>Sign In</button></li>
+                    {userState ? (
+                            <li><button className={styles.listEl} onClick={handleSignin}>Log out</button></li> 
+                        ) : (
+                            <>
+                                <li><button className={styles.listEl} onClick={handleLogin}>Log in</button></li>
+                                <li><button className={styles.listEl} onClick={handleSignin}>Sign In</button></li>
+                            </>
+                        )
+                    }
                 </ul>
             </div>
             <div className={styles.line}></div>
