f770fe29b67cae031a415f1dee0157b368e9411f
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-10-04 16:07:41 +0200

    Aggiunta sezione Admin + stile pagina

--- server/controllers.js
+++ server/controllers.js
@@ -59,9 +59,10 @@ export async function logout(req , res){
 
 export async function getAllUser(req , res){
     try {
-        const data = await db.many(`SELECT * FROM User`);
+        const data = await db.many(`SELECT * FROM Users`);
         console.log('Utenti recuperati');
-        res.status(200).json({data})
+        console.log(data);
+        res.status(200).json(data)
     } catch (err) {
         console.error('Errore in getAllUser');
         throw new Error({message : 'Errore nel recupero degli utenti'}); 
@@ -71,7 +72,7 @@ export async function getAllUser(req , res){
 export async function getOneUser(req, res , id){
     try {
         const data = await db.one(`SELECT * FROM Users WHERE id=$1` , id)
-        res.status(200).json({data})
+        res.status(200).json(data)
     } catch (error) {
         console.error('Errore in getOneUser');
         throw new Error({message : 'Utente non trovato'})
--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,16 +1,73 @@
+import { useEffect, useState } from "react"
+import styles from './admin.module.css'
 export function Admin(){
 
-    //fetch di all user
+    const [users , setUsers] = useState([])
+    
+    async function getAllUser(){
+        try {
+            const res = await fetch('http://localhost:3000/api/allUser')
+            const data = await res.json()
+            if(data){
+                setUsers(data)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    //funzione di deleteuser
+    useEffect(()=>{
+        getAllUser()
+    },[])
+
+    function handleUser(e){
+        e.preventDefault()
+    }
 
     return (
-        
-        <div>
-            <p>Admin Page</p>
+        <div className={styles.generalContainer}>
             <div>
-                {/* map di tutti gli utenti + img finta + tasto delete con funzione*/}
+                <p className={styles.title}>Admin Page</p>    
             </div>
+            <div className={styles.container}>
+                <div className={styles.userDetailContainer}>
+                    <p className={styles.subTitle}>Current Users</p> 
+                    <div className={styles.line}></div>  
+                    <div className={styles.userContainer}>
+                        {users && users.map((user) => {
+                            return (
+                                <div className={styles.detailContainer} key={user.id}>
+                                    <div>
+                                        <img alt="user img" src="/public/img/utente.jpeg"/>
+                                        <p>id: {user.id}</p>
+                                        <p>email: {user.email}</p>
+                                    </div>
+                                    <button>Delete</button>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className={styles.line}></div>
+                </div>
+                <div className={styles.formContainer}>
+                    <p className={styles.subTitle}>Add new user</p>
+                    <form onSubmit={handleUser} className={styles.detailForm}>
+                            <label htmlFor="email">email :</label>
+                        <div>
+                            <input type="text" name="email"/>
+                        </div>
+                            <label htmlFor="password">password :</label>
+                        <div>
+                            <input type="password" name="password"/>
+                        </div>
+                        <div>
+                            <label htmlFor="isAdmin">E' un admin?</label>
+                            <input type="checkbox" name="isAdmin"/>
+                        </div>
+                        <button>Invia</button>
+                    </form>
+                </div>
+            </div>        
         </div>
         
     )
